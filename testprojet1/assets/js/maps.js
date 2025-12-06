58189c78284a408af114e4a7064d87aa71b64ddf
THAUT627 <theo.rakotomavo@gmail.com>
2025-12-06 11:03:48 +0100

    Changer path AI lavadefense v2

--- testprojet1/assets/js/maps.js
+++ testprojet1/assets/js/maps.js
@@ -62,17 +62,18 @@ tinydefence.maps = [
         // Readable name for this map/level
         name: 'Lava Defense',
         // Start point for the enemies
-        start: {x: 1, y: 3},
+        start: {x: 0, y: 3},
         // End point for the enemies
         end: {x: 29, y: 1},
         // Waves
         waves: [
-            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 15,  enemySpeed: 30,  points: 10, type: 'crab'},
-            {dropInMillis: 2000, maxEnemies: 1,  enemyHealth: 100, enemySpeed: 40,  points: 30, type: 'crab'},
-            {dropInMillis: 650,  maxEnemies: 30, enemyHealth: 5,   enemySpeed: 100, points: 2,  type: 'crab'},
-            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15,  enemySpeed: 60,  points: 5,  type: 'crab'},
-            {dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20,  enemySpeed: 60,  points: 5,  type: 'crab'},
-            {dropInMillis: 800,  maxEnemies: 18, enemyHealth: 20,  enemySpeed: 60,  points: 5,  type: 'crab'},
+            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 10, enemySpeed: 20, points: 5, type: 'enemy'},
+            {dropInMillis: 1500, maxEnemies: 4,  enemyHealth: 20, enemySpeed: 40, points: 5, type: 'enemy'},
+            {dropInMillis: 2000, maxEnemies: 6,  enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
+            {dropInMillis: 1500, maxEnemies: 8,  enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
+            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
+            {dropInMillis: 1000, maxEnemies: 15, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
+            {dropInMillis: 800,  maxEnemies: 20, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
             // Add more waves here
         ]
     },
