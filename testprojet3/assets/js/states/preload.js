93635d75683cc40fbe71a44a6b44585a1158351a
THAUT627 <theo.rakotomavo@gmail.com>
2025-12-13 14:50:18 +0100

    fff

--- testprojet3/assets/js/states/preload.js
+++ testprojet3/assets/js/states/preload.js
@@ -5,33 +5,67 @@ tinydefence.preload = function (game) { };
 tinydefence.preload.prototype = {
 
 	preload: function () {
-		this.game.load.image('logo', 'testprojet2/assets/images/logo.png');
-		this.game.load.spritesheet('selection', 'testprojet2/assets/images/selection.png', 16, 16);
-		this.game.load.spritesheet('enemy', 'testprojet2/assets/images/enemy.png', 16, 16);
-		this.game.load.spritesheet('crab', 'testprojet2/assets/images/enemyCrab.png', 16, 16);
+		this.game.load.image('logo', 'testprojet3/assets/images/logo.png');
+		this.game.load.spritesheet('selection', 'testprojet3/assets/images/selection.png', 16, 16);
+		this.game.load.spritesheet('enemy', 'testprojet3/assets/images/enemy.png', 16, 16);
+		this.game.load.spritesheet('crab', 'testprojet3/assets/images/enemyCrab.png', 16, 16);
 
-		this.game.load.image('buildmenu', 'testprojet2/assets/images/ui/menuElements.png');
-		this.game.load.spritesheet('buildmenuButtons', 'testprojet2/assets/images/ui/menuButtons.png', 16, 16);
+		this.game.load.image('buildmenu', 'testprojet3/assets/images/ui/menuElements.png');
+		this.game.load.spritesheet('buildmenuButtons', 'testprojet3/assets/images/ui/menuButtons.png', 16, 16);
 
-		this.game.load.spritesheet('buttonCoverage', 'testprojet2/assets/images/ui/buttonCoverage.png', 32, 18);
-		this.game.load.spritesheet('buttonLevel', 'testprojet2/assets/images/ui/buttonLevel.png', 225, 35);
-		this.game.load.spritesheet('buttonMenuNav', 'testprojet2/assets/images/ui/buttonMenuNav.png', 20, 18);
+		this.game.load.spritesheet('buttonCoverage', 'testprojet3/assets/images/ui/buttonCoverage.png', 32, 18);
+		this.game.load.spritesheet('buttonLevel', 'testprojet3/assets/images/ui/buttonLevel.png', 225, 35);
+		this.game.load.spritesheet('buttonMenuNav', 'testprojet3/assets/images/ui/buttonMenuNav.png', 20, 18);
 
 		this.game.load.bitmapFont('font_white',
-			'testprojet2/assets/fonts/font.png',
-			'testprojet2/assets/fonts/font.fnt');
+			'testprojet3/assets/fonts/font.png',
+			'testprojet3/assets/fonts/font.fnt');
 		this.game.load.bitmapFont('font_green',
-			'testprojet2/assets/fonts/font_green.png',
-			'testprojet2/assets/fonts/font_green.fnt');
+			'testprojet3/assets/fonts/font_green.png',
+			'testprojet3/assets/fonts/font_green.fnt');
 		this.game.load.bitmapFont('font_red',
-			'testprojet2/assets/fonts/font_red.png',
-			'testprojet2/assets/fonts/font_red.fnt');
+			'testprojet3/assets/fonts/font_red.png',
+			'testprojet3/assets/fonts/font_red.fnt');
 
-		this.game.load.audio("background_music", "testprojet2/assets/audio/bgm.mp3");
+		this.game.load.audio("background_music", "testprojet3/assets/audio/bgm.mp3");
+		// Load all defined maps in maps.js
 		// Load all defined maps in maps.js
 		tinydefence.maps.forEach(map => {
-			this.game.load.tilemap(map.key, map.data, null, Phaser.Tilemap.TILED_JSON);
-			this.game.load.image(map.key + '_sprites', map.sprite);
+
+			// Case: map with variants (ex: LavaDefense)
+			if (map.variants && map.variants.length > 0) {
+
+				map.variants.forEach(variant => {
+
+					this.game.load.tilemap(
+						variant.key,
+						variant.data,
+						null,
+						Phaser.Tilemap.TILED_JSON
+					);
+
+					this.game.load.image(
+						variant.key + '_sprites',
+						variant.sprite
+					);
+				});
+
+			}
+			// Case: simple map (level1, BeachDefense)
+			else {
+
+				this.game.load.tilemap(
+					map.key,
+					map.data,
+					null,
+					Phaser.Tilemap.TILED_JSON
+				);
+
+				this.game.load.image(
+					map.key + '_sprites',
+					map.sprite
+				);
+			}
 		});
 
 		// Load all tower assets
