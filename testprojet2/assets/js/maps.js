fb3015b61473da9e8c55964ea96ccbe1a8908507
THAUT627 <theo.rakotomavo@gmail.com>
2025-12-13 20:07:29 +0100

    add map V2

--- testprojet2/assets/js/maps.js
+++ testprojet2/assets/js/maps.js
@@ -2,7 +2,7 @@ var tinydefence = tinydefence || {};
 
 
 tinydefence.maps = [
-    
+
     {
         // Key for this map
         key: 'level1',
@@ -13,18 +13,18 @@ tinydefence.maps = [
         // Readable name for this map/level
         name: 'Level 1',
         // Start point for the enemies
-        start: {x: 0, y: 1},
+        start: { x: 0, y: 1 },
         // End point for the enemies
-        end: {x: 16, y: 14},
+        end: { x: 16, y: 14 },
         // Waves
         waves: [
-            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 10, enemySpeed: 20, points: 5, type: 'enemy'},
-            {dropInMillis: 1500, maxEnemies: 4,  enemyHealth: 20, enemySpeed: 40, points: 5, type: 'enemy'},
-            {dropInMillis: 2000, maxEnemies: 6,  enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
-            {dropInMillis: 1500, maxEnemies: 8,  enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
-            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
-            {dropInMillis: 1000, maxEnemies: 15, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
-            {dropInMillis: 800,  maxEnemies: 20, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
+            { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 10, enemySpeed: 20, points: 5, type: 'enemy' },
+            { dropInMillis: 1500, maxEnemies: 4, enemyHealth: 20, enemySpeed: 40, points: 5, type: 'enemy' },
+            { dropInMillis: 2000, maxEnemies: 6, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1500, maxEnemies: 8, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1000, maxEnemies: 15, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 800, maxEnemies: 20, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
             // Add more waves here
         ]
     },
@@ -38,22 +38,22 @@ tinydefence.maps = [
         // Readable name for this map/level
         name: 'Beach Defense',
         // Start point for the enemies
-        start: {x: 1, y: 10},
+        start: { x: 1, y: 10 },
         // End point for the enemies
-        end: {x: 28, y: 10},
+        end: { x: 28, y: 10 },
         // Waves
         waves: [
-            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 15,  enemySpeed: 30,  points: 10, type: 'crab'},
-            {dropInMillis: 2000, maxEnemies: 1,  enemyHealth: 100, enemySpeed: 40,  points: 30, type: 'crab'},
-            {dropInMillis: 650,  maxEnemies: 30, enemyHealth: 5,   enemySpeed: 100, points: 2,  type: 'crab'},
-            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15,  enemySpeed: 60,  points: 5,  type: 'crab'},
-            {dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20,  enemySpeed: 60,  points: 5,  type: 'crab'},
-            {dropInMillis: 800,  maxEnemies: 18, enemyHealth: 20,  enemySpeed: 60,  points: 5,  type: 'crab'},
+            { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 15, enemySpeed: 30, points: 10, type: 'crab' },
+            { dropInMillis: 2000, maxEnemies: 1, enemyHealth: 100, enemySpeed: 40, points: 30, type: 'crab' },
+            { dropInMillis: 650, maxEnemies: 30, enemyHealth: 5, enemySpeed: 100, points: 2, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 800, maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
             // Add more waves here
         ]
     },
     {
-    // Key for this map
+        // Key for this map
         key: 'LavaDefense',
         // Path to map data
         data: 'testprojet2/assets/maps/LavaDefense/map.json',
@@ -62,20 +62,38 @@ tinydefence.maps = [
         // Readable name for this map/level
         name: 'Lava Defense',
         // Start point for the enemies
-        start: {x: 0, y: 3},
+        start: { x: 0, y: 3 },
         // End point for the enemies
-        end: {x: 29, y: 1},
+        end: { x: 29, y: 1 },
         // Waves
         waves: [
-            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 10, enemySpeed: 20, points: 5, type: 'enemy'},
-            {dropInMillis: 1500, maxEnemies: 4,  enemyHealth: 20, enemySpeed: 40, points: 5, type: 'enemy'},
-            {dropInMillis: 2000, maxEnemies: 6,  enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
-            {dropInMillis: 1500, maxEnemies: 8,  enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
-            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
-            {dropInMillis: 1000, maxEnemies: 15, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
-            {dropInMillis: 800,  maxEnemies: 20, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy'},
+            { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 10, enemySpeed: 20, points: 5, type: 'enemy' },
+            { dropInMillis: 1500, maxEnemies: 4, enemyHealth: 20, enemySpeed: 40, points: 5, type: 'enemy' },
+            { dropInMillis: 2000, maxEnemies: 6, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1500, maxEnemies: 8, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1000, maxEnemies: 15, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 800, maxEnemies: 20, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
             // Add more waves here
         ]
     },
-    
+    {
+        key: 'LavaDefense2',
+        data: 'testprojet3/assets/maps/LavaDefense/map1.json',
+        sprite: 'testprojet3/assets/maps/LavaDefense/map1.png',
+        name: "Lava Defense 2",
+        start: { x: 0, y: 3 },
+        end: { x: 29, y: 1 },
+        waves: [
+            { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 10, enemySpeed: 20, points: 5, type: 'enemy' },
+            { dropInMillis: 1500, maxEnemies: 4, enemyHealth: 20, enemySpeed: 40, points: 5, type: 'enemy' },
+            { dropInMillis: 2000, maxEnemies: 6, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1500, maxEnemies: 8, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 1000, maxEnemies: 15, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            { dropInMillis: 800, maxEnemies: 20, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'enemy' },
+            // Add more waves here
+        ]
+    },
+
 ];
