ec7866e59f6b395b92918a947619cf01dc30cd24
THAUT627 <theo.rakotomavo@gmail.com>
2025-12-13 14:37:03 +0100

    Abandon des maps aleatoires ou voir plus tard

--- testprojet3/assets/js/config/maps.js
+++ testprojet3/assets/js/config/maps.js
@@ -53,59 +53,72 @@ tinydefence.maps = [
         ]
     },
     {
-        key: "LavaDefense",
-        name: "Lava Defense",
-        variants: [
-            {
-                key: 'LavaDefense_0',
-                data: 'testprojet3/assets/maps/LavaDefense/map.json',
-                sprite: 'testprojet3/assets/maps/LavaDefense/map.png',
-                start: { x: 1, y: 3 },
-                end: { x: 29, y: 1 },
-                waves: [
-                    { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 15, enemySpeed: 30, points: 10, type: 'crab' },
-                    { dropInMillis: 2000, maxEnemies: 1, enemyHealth: 100, enemySpeed: 40, points: 30, type: 'crab' },
-                    { dropInMillis: 650, maxEnemies: 30, enemyHealth: 5, enemySpeed: 100, points: 2, type: 'crab' },
-                    { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab' },
-                    { dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
-                    { dropInMillis: 800, maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
-                    // Add more waves here
-                ]
-            },
-            {
-                key: 'LavaDefense_1',
-                data: 'testprojet3/assets/maps/LavaDefense/map1.json',
-                sprite: 'testprojet3/assets/maps/LavaDefense/map1.png',
-                start: { x: 1, y: 3 },
-                end: { x: 29, y: 1 },
-                waves: [
-                    { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 15, enemySpeed: 30, points: 10, type: 'crab' },
-                    { dropInMillis: 2000, maxEnemies: 1, enemyHealth: 100, enemySpeed: 40, points: 30, type: 'crab' },
-                    { dropInMillis: 650, maxEnemies: 30, enemyHealth: 5, enemySpeed: 100, points: 2, type: 'crab' },
-                    { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab' },
-                    { dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
-                    { dropInMillis: 800, maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
-                    // Add more waves here
-                ]
-            },
-            {
-                key: 'LavaDefense_2',
-                data: 'testprojet3/assets/maps/LavaDefense/map2.json',
-                sprite: 'testprojet3/assets/maps/LavaDefense/map2.png',
-                start: { x: 1, y: 3 },
-                end: { x: 29, y: 1 },
-                waves: [
-                    { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 15, enemySpeed: 30, points: 10, type: 'crab' },
-                    { dropInMillis: 2000, maxEnemies: 1, enemyHealth: 100, enemySpeed: 40, points: 30, type: 'crab' },
-                    { dropInMillis: 650, maxEnemies: 30, enemyHealth: 5, enemySpeed: 100, points: 2, type: 'crab' },
-                    { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab' },
-                    { dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
-                    { dropInMillis: 800, maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
-                    // Add more waves here
-                ]
-            }
-        ],
-        
-
+        key: 'LavaDefense_0',
+        data: 'testprojet3/assets/maps/LavaDefense/map.json',
+        sprite: 'testprojet3/assets/maps/LavaDefense/map.png',
+        name: "Lava Defense 1",
+        start: { x: 1, y: 3 },
+        end: { x: 29, y: 1 },
+        waves: [
+            { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 15, enemySpeed: 30, points: 10, type: 'crab' },
+            { dropInMillis: 2000, maxEnemies: 1, enemyHealth: 100, enemySpeed: 40, points: 30, type: 'crab' },
+            { dropInMillis: 650, maxEnemies: 30, enemyHealth: 5, enemySpeed: 100, points: 2, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 800, maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            // Add more waves here
+        ]
+    },
+    {
+        key: 'LavaDefense_1',
+        data: 'testprojet3/assets/maps/LavaDefense/map1.json',
+        sprite: 'testprojet3/assets/maps/LavaDefense/map1.png',
+        name: "Lava Defense 2",
+        start: { x: 1, y: 3 },
+        end: { x: 29, y: 1 },
+        waves: [
+            { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 15, enemySpeed: 30, points: 10, type: 'crab' },
+            { dropInMillis: 2000, maxEnemies: 1, enemyHealth: 100, enemySpeed: 40, points: 30, type: 'crab' },
+            { dropInMillis: 650, maxEnemies: 30, enemyHealth: 5, enemySpeed: 100, points: 2, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 800, maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            // Add more waves here
+        ]
+    },
+    {
+        key: 'LavaDefense_2',
+        data: 'testprojet3/assets/maps/LavaDefense/map2.json',
+        sprite: 'testprojet3/assets/maps/LavaDefense/map2.png',
+        name: "Lava Defense 3",
+        start: { x: 1, y: 3 },
+        end: { x: 29, y: 1 },
+        waves: [
+            { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 15, enemySpeed: 30, points: 10, type: 'crab' },
+            { dropInMillis: 2000, maxEnemies: 1, enemyHealth: 100, enemySpeed: 40, points: 30, type: 'crab' },
+            { dropInMillis: 650, maxEnemies: 30, enemyHealth: 5, enemySpeed: 100, points: 2, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 800, maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            // Add more waves here
+        ]
+    },
+    {
+        key: 'LavaDefense_3',
+        data: 'testprojet3/assets/maps/LavaDefense/map2.json',
+        sprite: 'testprojet3/assets/maps/LavaDefense/map2.png',
+        name: "Lava Defense 4",
+        start: { x: 1, y: 3 },
+        end: { x: 29, y: 1 },
+        waves: [
+            { dropInMillis: 1500, maxEnemies: 2, enemyHealth: 15, enemySpeed: 30, points: 10, type: 'crab' },
+            { dropInMillis: 2000, maxEnemies: 1, enemyHealth: 100, enemySpeed: 40, points: 30, type: 'crab' },
+            { dropInMillis: 650, maxEnemies: 30, enemyHealth: 5, enemySpeed: 100, points: 2, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            { dropInMillis: 800, maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab' },
+            // Add more waves here
+        ]
     }
+
 ];
